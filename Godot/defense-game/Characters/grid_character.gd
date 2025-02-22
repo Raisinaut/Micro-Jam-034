a902342a8d56260d4676d5f3c28fdfec9ccaeb7b
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2025-02-21 21:02:40 -0500

    player, lerp position

--- Godot/defense-game/Characters/grid_character.gd
+++ Godot/defense-game/Characters/grid_character.gd
@@ -4,22 +4,21 @@ extends Node2D
 var _grid_step = 100
 var _grid_origin = Vector2(0,0)
 var _grid_position = Vector2(0,0)
+var move_direction = Vector2(0,0)
 
-func _ready() -> void:
-	pass
+func _process(delta: float) -> void:
+	move()
+	update_position(delta)
 
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("move_up"): _grid_position.y -= 1
-	if Input.is_action_just_pressed("move_down"): _grid_position.y += 1
-	if Input.is_action_just_pressed("move_left"): _grid_position.x -= 1
-	if Input.is_action_just_pressed("move_right"): _grid_position.x += 1
-	update_position()
+func move():
+	_grid_position += move_direction
+	move_direction = Vector2.ZERO
 
-func update_position():
-	position = (_grid_position * _grid_step) + _grid_origin
+func update_position(delta: float):
+	position = lerp(position, (_grid_position * _grid_step) + _grid_origin, delta * 20)
 
 func initialize_grid(grid_size: float, grid_origin: Vector2):
 	_grid_step = grid_size
 	_grid_origin = grid_origin
 	_grid_position = (position - _grid_origin) / _grid_step
-	update_position()
+	update_position(0)
